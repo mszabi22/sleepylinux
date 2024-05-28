1915ac754fb345f992819065da904596d9facc5c
Major Szabolcs <majorsza@localhost.localdomain>
2024-05-28 08:33:49 +0200

    Add create custom iso style

--- debian-live-iso.sh
+++ debian-live-iso.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DATUM=`date +%Y.%m.%d.`
+DATUM=`date +%Y.%m.%d`
 mv SleepyLinux-amd64.hybrid.iso SleepyLinux-$DATUM-amd64.iso
 md5sum SleepyLinux-$DATUM-amd64.iso > SleepyLinux-$DATUM-amd64.iso.md5
