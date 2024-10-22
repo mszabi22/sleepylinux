fd40670660fd6bdb13b5ed1504a61c769dff3974
mszabi22 <majorsza@localhost.localdomain>
2024-10-22 13:00:31 +0200

    Update

--- debian-live-iso.sh
+++ debian-live-iso.sh
@@ -2,5 +2,3 @@
 DATUM=`date +%Y.%m.%d`
 mv SleepyLinux-amd64.hybrid.iso SleepyLinux-$DATUM-amd64.iso
 md5sum SleepyLinux-$DATUM-amd64.iso > SleepyLinux-$DATUM-amd64.iso.md5
-gpg -s -u majorsza@gmail.com SleepyLinux-$DATUM-amd64.iso
-gpg --verify SleepyLinux-$DATUM-amd64.iso.gpg
