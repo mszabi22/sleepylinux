f3997e82a47a6b1798a53cec460ba5db836328dc
mszabi <majorsza@gmail.com>
2024-12-18 08:16:20 +0100

    2024-12-18@0816

--- debian-live-build.sh
+++ debian-live-build.sh
@@ -20,9 +20,9 @@ lb config noauto \
 	--iso-publisher "Sleepy.hu" \
 	--memtest none \
 	--interactive shell
-
+ 
 echo "mc lshw wireshark network-manager net-tools wget openssh-client \
 task-xfce-desktop xserver-xorg-core gparted ntfs-3g hfsprogs rsync dosfstools \
-partclone clonezilla sshfs nano calamares calamares-settings-debian" > config/package-lists/my.list.chroot
+partclone clonezilla sshfs nano calamares calamares-settings-debian fish" > config/package-lists/my.list.chroot
 # # #
 lb build
