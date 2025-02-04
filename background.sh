ae90b959194de81fe1f939201b54616c1e9825ba
mszabi22 <majorsza@localhost.localdomain>
2025-02-04 15:07:22 +0100

    2025-02-04@1507

--- background.sh
+++ background.sh
@@ -8,15 +8,15 @@ echo "
     <file>
       <size width="1280" height="800">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
       <size width="1280" height="1024">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
-      <size width="1600" height="1200">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
-      <size width="1920" height="1080">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
-      <size width="1920" height="1200">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
-      <size width="2560" height="1440">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
-      <size width="2560" height="1600">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
-      <size width="3200" height="1800">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
-      <size width="3200" height="2000">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
-      <size width="3840" height="2160">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
-      <size width="5120" height="2880">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size></size>
+      <size width="1600" height="1200">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
+      <size width="1920" height="1080">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
+      <size width="1920" height="1200">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
+      <size width="2560" height="1440">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
+      <size width="2560" height="1600">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
+      <size width="3200" height="1800">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
+      <size width="3200" height="2000">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
+      <size width="3840" height="2160">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
+      <size width="5120" height="2880">/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/background.svg</size>
     </file>
   </static>
 </background>" > /usr/share/images/desktop-base/desktop-background.xml
