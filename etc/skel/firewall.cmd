2c40f40d91dfa37821e9fc9f2650099b24af9b10
mszabi22 <majorsza@localhost.localdomain>
2025-02-19 13:57:36 +0100

    2025-02-19@1357

---
+++ etc/skel/firewall.cmd
@@ -0,0 +1,9 @@
+firewall-cmd --set-default-zone=drop
+firewall-cmd --permanent --direct --add-rule ipv4 filter OUTPUT 0 -j ACCEPT
+firewall-cmd --permanent --zone=drop --add-service=ssh
+firewall-cmd --permanent --zone=drop --add-port=5678/udp --add-port=8291/tcp
+firewall-cmd --permanent --zone=drop --add-port=68/udp --add-port=67/udp
+firewall-cmd --permanent --add-icmp-block-inversion
+firewall-cmd --permanent --add-icmp-block=echo-reply
+firewall-cmd --permanent --add-icmp-block=echo-request
+firewall-cmd --reload
