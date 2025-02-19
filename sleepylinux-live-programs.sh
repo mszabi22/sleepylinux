2742ea532860d1ecca9a64ae6b054d8baf7be4e8
mszabi22 <majorsza@localhost.localdomain>
2025-02-19 12:52:32 +0100

    2025-02-19@1252

--- sleepylinux-live-programs.sh
+++ sleepylinux-live-programs.sh
@@ -57,6 +57,7 @@ firewall-cmd --set-default-zone=drop
 firewall-cmd --permanent --direct --add-rule ipv4 filter OUTPUT 0 -j ACCEPT
 firewall-cmd --permanent --zone=drop --add-service=ssh
 firewall-cmd --permanent --zone=drop --add-port=5678/udp --add-port=8291/tcp
+firewall-cmd --permanent --zone=drop --add-port=68/udp --add-port=67/udp
 firewall-cmd --reload
 
 echo -e "${yellow}Admin Tools? (i/n)${white}"
