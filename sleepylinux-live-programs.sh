294d56d33ca89ff4dd2fb1d26e7d364465c50d75
mszabi22 <majorsza@localhost.localdomain>
2025-02-19 12:38:34 +0100

    2025-02-19@1238

--- sleepylinux-live-programs.sh
+++ sleepylinux-live-programs.sh
@@ -56,7 +56,6 @@ apt install -y mc sudo ssh cups printer-driver-cups-pdf gvfs-fuse gvfs-backends 
 firewall-cmd --set-default-zone=drop
 firewall-cmd --permanent --direct --add-rule ipv4 filter OUTPUT 0 -j ACCEPT
 firewall-cmd --permanent --zone=drop --add-service=ssh
-firewall-cmd --permanent --zone=drop --add-service=wireguard
 firewall-cmd --permanent --zone=drop --add-port=5678/udp --add-port=8291/tcp
 firewall-cmd --reload
 
