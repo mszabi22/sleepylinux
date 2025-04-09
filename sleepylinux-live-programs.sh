1c50bb6f17b4dc2a0ba9bac52047770867761f23
mszabi22 <majorsza@localhost.localdomain>
2025-04-09 10:05:18 +0200

    2025-04-09@1005

--- sleepylinux-live-programs.sh
+++ sleepylinux-live-programs.sh
@@ -54,11 +54,14 @@ apt install -y mc sudo ssh cups printer-driver-cups-pdf gvfs-fuse gvfs-backends 
     gimp simple-scan gnupg gnupg2 gnupg1 eog zstd imagemagick alacarte gocryptfs mugshot keepassxc tor geany ntpsec zenity\
     libpam-google-authenticator gnome-system-tools wireguard wireguard-tools libreoffice-l10n-hu gnome-online-accounts \
     syncthing qrencode ecryptfs-utils audacious molly-guard kleopatra deluge borgbackup vorta ssh-askpass clamtk mpv smplayer \
-    ntpdate firewalld firewall-config firewall-applet krita krita-l10n nfs-common
+    ntpdate firewalld firewall-config firewall-applet krita krita-l10n nfs-common flatpak
     #ttf-mscorefonts-installer
 
 echo "%sudo ALL = (ALL:ALL) NOPASSWD: /usr/bin/firewall-cmd" >> /etc/sudoers
 
+
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install -y com.obsproject.Studio
 ########################################################################
 echo -e "${yellow}Admin Tools? (i/n)${white}"
 read ADMINTOOLS_INSTALL
