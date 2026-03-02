2e6e48151b5b575b10cf99ef092891d07ad5e706
Szabi <majorsza@gmail.com>
2026-03-02 09:56:42 +0100

    2026-03-02@0956

---
+++ build/add-programs.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+# # # Run this second in chroot # # #
+green='\e[0;32m'
+blue='\e[0;34m'
+yellow='\e[0;33m'
+white='\e[0;37m'
+########################################################################
+echo -e "${yellow}rc-local setting...${white}"
+echo '[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+ 
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/rc-local.service
+
+echo '#!/bin/sh -e
+exit 0
+' > /etc/rc.local
+
+chmod +x /etc/rc.local
+systemctl enable rc-local
+########################################################################
+echo -e "${yellow}Repository setting...${white}"
+echo "deb https://ftp.debian.org/debian/ trixie contrib main non-free non-free-firmware contrib
+deb https://ftp.debian.org/debian/ trixie-updates contrib main non-free non-free-firmware contrib
+deb https://ftp.debian.org/debian/ trixie-proposed-updates contrib main non-free non-free-firmware contrib
+deb https://ftp.debian.org/debian/ trixie-backports contrib main non-free non-free-firmware contrib
+deb https://security.debian.org/debian-security/ trixie-security contrib main non-free non-free-firmware contrib" > /etc/apt/sources.list
+apt update
+apt upgrade -y
+
+########################################################################
+echo -e "${yellow}Apps install...${white}"
+apt install -y mc sudo ssh cups printer-driver-cups-pdf gvfs-fuse gvfs-backends apt-transport-https rsync curl wget \
+    firmware-iwlwifi firmware-atheros firmware-brcm80211 blueman ttf-mscorefonts-installer vlc thunderbird thunderbird-l10n-hu \
+    gimp simple-scan gnupg gnupg2 gnupg1 eog zstd imagemagick alacarte gocryptfs mugshot keepassxc tor geany ntpsec \
+    libpam-google-authenticator gnome-system-tools wireguard wireguard-tools libreoffice-l10n-hu gnome-online-accounts \
+    syncthing qrencode ecryptfs-utils audacious molly-guard kleopatra deluge mpv smplayer \
+    obs-studio ffmpeg bleachbit flatpak handbrake gnome-calendar greybird-gtk-theme elementary-xfce-icon-theme adwaita-icon-theme \
+	tango-icon-theme xfwm4-theme-breeze htop btop linux-cpupower cpupower-gui onionshare fastfetch birdtray git
+########################################################################
+echo -e "${yellow}LocalSend install...${white}"
+wget https://github.com/localsend/localsend/releases/download/v1.17.0/LocalSend-1.17.0-linux-x86-64.deb
+dpkg -i LocalSend-1.17.0-linux-x86-64.deb
+apt-get -f install -y
+rm LocalSend-1.17.0-linux-x86-64.deb        
+########################################################################
+echo -e "${yellow}Admin Tools? (i/n)${white}"
+read ADMINTOOLS_INSTALL
+if [ $ADMINTOOLS_INSTALL = 'i' ]; then
+echo -e "${yellow}Admin Tools telepítése...${white}"
+	apt install gprename gparted netdiscover sshuttle grub-customizer remmina remmina-plugin-rdp remmina-plugin-vnc \
+	net-tools dnsutils arping zenity chntpw hardinfo acpidump acpidump sysbench dislocker stress s-tui traceroute iputils-ping \
+	wireshark tigervnc-viewer tigervnc-tools
+	
+	echo -e "${yellow}Winbox install...${white}"
+	cd /opt
+	mkdir winbox
+	cd winbox
+	wget https://download.mikrotik.com/routeros/winbox/4.0.1/WinBox_Linux.zip
+	unzip WinBox_Linux.zip
+	ln -s /opt/winbox/WinBox /usr/local/bin/winbox
+	ln -s /opt/winbox/assets/img/winbox.png /usr/share/icons/winbox.png
+	rm WinBox_Linux.zip
+	chmod -R 777 /opt/winbox
+	
+echo "[Desktop Entry]
+Name=Winbox
+Exec=/usr/local/bin/winbox
+Icon=/usr/share/icons/winbox.png
+ 
+Encoding=UTF-8
+ExecutionMode=normal
+Type=Application
+Categories=Application;Network;
+Enabled=true" > /usr/share/applications/winbox.desktop
+	
+fi
+########################################################################
+echo -e "${yellow}SSH setting...${white}"
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+echo "Include /etc/ssh/sshd_config.d/*.conf
+Port 22
+AllowUsers majorsza@*
+PermitRootLogin no
+PubkeyAuthentication yes
+AuthorizedKeysFile	.ssh/authorized_keys .ssh/authorized_keys2
+PasswordAuthentication no
+
+KbdInteractiveAuthentication yes
+UsePAM yes
+ChallengeResponseAuthentication yes
+
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem	sftp	/usr/lib/openssh/sftp-server" > /etc/ssh/sshd_config
+sudo apt install libpam-google-authenticator
+cp /etc/pam.d/sshd /root/pam.d_sshd.bak
+echo "
+# two-factor authentication via Google Authenticator
+auth   required   pam_google_authenticator.so" >> /etc/pam.d/sshd
+/etc/init.d/ssh restart
+
+########################################################################
+echo -e "${yellow}Brave install...${white}"    
+apt install curl
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+apt install brave-browser -y
+echo "[Desktop Entry]
+Version=1.0
+Name=Brave Webböngésző
+Exec=/usr/bin/brave-browser-stable %U
+StartupNotify=true
+Terminal=false
+Icon=brave-browser
+Type=Application
+Categories=Network;WebBrowser;
+MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ipfs;x-scheme-handler/ipns;
+Actions=new-window;new-private-window;
+
+[Desktop Action new-window]
+Name=Új ablak
+Exec=/usr/bin/brave-browser-stable
+
+[Desktop Action new-private-window]
+Name=Inkognitó mód
+Exec=/usr/bin/brave-browser-stable --incognito
+" > /usr/share/applications/brave-browser.desktop
+
+########################################################################
+echo -e "${yellow}Tor Browser install...${white}"
+sudo apt install jq -y
+TOR_VERZIO="$(curl -s https://aus1.torproject.org/torbrowser/update_3/release/downloads.json | jq -r ".version")"
+cd /opt
+wget https://www.torproject.org/dist/torbrowser/$TOR_VERZIO/tor-browser-linux-x86_64-$TOR_VERZIO.tar.xz
+tar -xvJf tor-browser-*.tar.xz
+rm tor-browser-*.tar.xz
+chmod -R 755 /opt/tor-browser 
+echo "#!/bin/bash
+sudo chown -R $USER:$USER /opt/tor-browser
+cd /opt/tor-browser
+./start-tor-browser.desktop --register-app" > /usr/local/bin/tor-browser-setup
+chmod +x /usr/local/bin/tor-browser-setup
+cd
+
+########################################################################
+echo -e "${yellow}Signal install...${white}"
+cd
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | tee /etc/apt/sources.list.d/signal-xenial.list
+apt update && sudo apt install signal-desktop
+rm signal-desktop-keyring.gpg
+########################################################################
+echo -e "${yellow}Viber install...${white}"
+cd /usr/local/bin
+wget -O viber https://download.cdn.viber.com/desktop/Linux/viber.AppImage
+chmod +x viber
+
+cd /usr/share/icons
+wget -O viber-logo.png https://www.freepnglogos.com/uploads/viber-png/viber-top-way-video-call-faster-full-tronzi-6.png
+
+echo "[Desktop Entry]
+Name=Viber
+Exec=/usr/local/bin/viber
+Icon=/usr/share/icons/viber-logo.png
+
+Encoding=UTF-8
+ExecutionMode=normal
+Type=Application
+Categories=Network;
+Enabled=true" > /usr/share/applications/viber.desktop
+########################################################################
+echo -e "${yellow}Standard Notes install...${white}"
+cd /usr/share/icons
+wget https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/standard-notes.png
+cd /usr/local/bin
+wget -O standard-notes https://github.com/standardnotes/app/releases/download/%40standardnotes/desktop%403.201.18/standard-notes-3.201.18-linux-x86_64.AppImage
+chmod a+x standard-notes
+echo "[Desktop Entry]
+Name=Standard Notes
+Exec=/usr/local/bin/standard-notes
+Icon=/usr/share/icons/standard-notes.png
+
+Encoding=UTF-8
+ExecutionMode=normal
+Type=Application
+Categories=Office;
+Enabled=true" > /usr/share/applications/standard-notes.desktop
+########################################################################
+echo -e "${yellow}AEScrypt install...${white}"
+cd
+wget https://www.aescrypt.com/download/v4/linux/aescrypt_gui-4.5.0-Linux-x86_64.deb
+dpkg -i aescrypt_gui-*.deb
+rm aescrypt_gui-*.deb
+########################################################################
+echo -e "${yellow}VeraCrypt install...${white}"
+if [ `cat /etc/issue | grep 12 | wc -l` = "1" ]; then
+	wget https://launchpad.net/veracrypt/trunk/1.26.24/+download/veracrypt-1.26.24-Debian-12-amd64.deb
+	dpkg -i veracrypt-1.26.24-Debian-12-amd64.deb
+	apt-get -f install -y
+fi
+
+if [ `cat /etc/issue | grep 13 | wc -l` = "1" ]; then
+	wget https://launchpad.net/veracrypt/trunk/1.26.24/+download/veracrypt-1.26.24-Debian-13-amd64.deb
+	dpkg -i veracrypt-1.26.24-Debian-13-amd64.deb
+	apt-get -f install -y
+fi
+echo "%sudo  ALL = (ALL:ALL) NOPASSWD: /usr/bin/veracrypt" >> /etc/sudoers
+
+########################################################################
+echo -e "${yellow}TeamViewer install...${white}"
+cd
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+dpkg -i teamviewer_amd64.deb
+rm teamviewer_amd64.deb
+########################################################################
+echo -e "${yellow}LibreOffice eltávolítása...${white}"
+sudo apt remove --purge libreoffice-*
+sudo apt autoremove
+sudo apt clean
+echo -e "${yellow}root passwd...${white}"
+passwd
+
+echo "Új LibreOffice install!!!"
---
+++ build/build-ciszterci.sh
@@ -0,0 +1,214 @@
+#!/bin/bash
+set -e
+
+WORKDIR="CL-SZMP"
+ISO_NAME="CiszterciLinux"
+
+apt-get update
+apt-get install -y live-build curl wget gnupg
+
+rm -rf $WORKDIR
+mkdir $WORKDIR
+cd $WORKDIR
+
+lb clean
+
+lb config noauto \
+  --mode debian \
+  --distribution trixie \
+  --architectures amd64 \
+  --linux-flavours amd64 \
+  --binary-images iso-hybrid \
+  --debian-installer none \
+  --archive-areas "main contrib non-free non-free-firmware" \
+  --debootstrap-options "--variant=minbase" \
+  --bootappend-live "boot=live username=live persistence components hostname=CiszterciLinux locales=hu_HU.UTF-8 keyboard-layouts=hu timezone=Europe/Budapest" \
+  --image-name "$ISO_NAME" \
+  --iso-application "$ISO_NAME" \
+  --iso-publisher "Sleepy.hu" \
+  --iso-volume "$ISO_NAME" \
+  --memtest none \
+  --interactive shell
+
+mkdir -p config/package-lists
+
+cat > config/package-lists/desktop.list.chroot <<EOF
+systemd-sysv
+live-boot
+live-config
+live-config-systemd
+adduser
+passwd
+login
+sudo
+task-xfce-desktop
+lightdm
+lightdm-gtk-greeter
+xserver-xorg-core
+network-manager
+mc nano wget curl rsync
+apt-utils dialog
+calamares calamares-settings-debian
+cups printer-driver-cups-pdf
+gvfs-fuse gvfs-backends
+firmware-iwlwifi firmware-atheros firmware-brcm80211
+blueman
+vlc thunderbird thunderbird-l10n-hu
+gimp simple-scan eog
+imagemagick zstd
+menulibre mugshot keepassxc geany
+ntpsec zenity
+gnupg gnupg2
+wireguard wireguard-tools
+libreoffice libreoffice-l10n-hu
+audacious mpv
+krita krita-l10n
+nfs-common
+veyon-service
+bash-completion screen
+samba smbclient cifs-utils
+clamtk
+openssh-server
+apt-transport-https
+ttf-mscorefonts-installer
+gnupg1
+libpam-google-authenticator
+gnome-system-tools
+gnome-online-accounts
+molly-guard
+kleopatra
+geogebra
+birdtray
+polkitd
+pkexec
+dbus-user-session
+accountsservice
+xfce4-session
+EOF
+
+# autologin
+mkdir -p config/includes.chroot/etc/lightdm/lightdm.conf.d
+cat > config/includes.chroot/etc/lightdm/lightdm.conf.d/01-autologin.conf <<EOF
+[Seat:*]
+autologin-user=live
+autologin-user-timeout=0
+EOF
+
+# CHROOT HOOK
+mkdir -p config/hooks/normal
+
+cat > config/hooks/normal/999-full.hook.chroot <<'EOF'
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | debconf-set-selections
+
+# repo
+cat > /etc/apt/sources.list <<REPO
+deb https://ftp.debian.org/debian trixie main contrib non-free non-free-firmware
+deb https://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
+deb https://ftp.debian.org/debian trixie-updates main contrib non-free non-free-firmware
+deb https://ftp.debian.org/debian trixie-backports main contrib non-free non-free-firmware
+REPO
+
+apt update
+apt -y upgrade
+
+# SSH hardening
+apt install -y openssh-server libpam-google-authenticator
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+cat > /etc/ssh/sshd_config <<SSH
+Port 22
+PermitRootLogin no
+PasswordAuthentication no
+PubkeyAuthentication yes
+KbdInteractiveAuthentication yes
+UsePAM yes
+ChallengeResponseAuthentication yes
+X11Forwarding yes
+Subsystem sftp /usr/lib/openssh/sftp-server
+SSH
+
+echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd
+
+# Brave
+curl -fsSLo /usr/share/keyrings/brave.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave.gpg] https://brave-browser-apt-release.s3.brave.com stable main" > /etc/apt/sources.list.d/brave.list
+apt update
+apt install -y brave-browser
+
+# Signal
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > /usr/share/keyrings/signal.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/signal.gpg] https://updates.signal.org/desktop/apt xenial main" > /etc/apt/sources.list.d/signal.list
+apt update
+apt install -y signal-desktop
+
+# TeamViewer
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+dpkg -i teamviewer_amd64.deb || apt -f install -y
+rm teamviewer_amd64.deb
+
+# AEScrypt install
+wget https://www.aescrypt.com/download/v4/linux/aescrypt_gui-4.5.0-Linux-x86_64.deb
+dpkg -i aescrypt_gui-*.deb
+rm aescrypt_gui-*.deb
+
+# VeraCrypt (Debian 13 = Trixie)
+wget https://launchpad.net/veracrypt/trunk/1.26.24/+download/veracrypt-1.26.24-Debian-13-amd64.deb
+dpkg -i veracrypt-1.26.24-Debian-13-amd64.deb || apt -f install -y
+
+echo "%sudo ALL=(ALL) NOPASSWD:/usr/bin/veracrypt" > /etc/sudoers.d/veracrypt
+chmod 440 /etc/sudoers.d/veracrypt
+
+# Samba share
+mkdir -p /home/scan
+chmod 2775 /home/scan
+groupadd -f smbshare
+chgrp smbshare /home/scan
+chgrp -R smbshare /home/scan
+chmod 2775 /home/scan
+useradd -M -s /usr/sbin/nologin scan
+usermod -aG smbshare scan
+
+
+cat >> /etc/samba/smb.conf <<SAMBA
+
+[scan]
+   path = /home/scan
+   writable = yes
+   guest ok = yes
+   force create mode = 775
+   force directory mode = 775
+SAMBA
+
+# Live user
+id -u live >/dev/null 2>&1 || useradd -m -s /bin/bash -G audio,cdrom,dip,floppy,video,plugdev,netdev,sudo live
+echo "live:live" | chpasswd
+chmod 700 /home/live
+chown -R live:live /home/live
+
+
+echo "== FULL BUILD DONE =="
+EOF
+
+chmod +x config/hooks/normal/999-full.hook.chroot
+
+echo
+echo "================================================="
+echo "INTERAKTÍV SHELL fog indulni."
+echo "Szerkeszd az /etc/skel mappát."
+echo "Kilépés: exit"
+echo "================================================="
+echo
+
+lb build
+
+echo
+echo "ISO kész:"
+ls -lh *.iso
+
+### javítás: ###
+#sudo chroot chroot /bin/bash
+#lb clean --binary
+#lb binary
