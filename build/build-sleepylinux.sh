e0add5362aa85bbe542d440e728b9c307763d0fe
Szabi <majorsza@gmail.com>
2026-03-06 07:42:52 +0100

    2026-03-06@0742

--- build/build-sleepylinux.sh
+++ build/build-sleepylinux.sh
@@ -264,6 +264,7 @@ echo
 lb build
 
 echo
+DATUM=`date +%Y.%m.%d`; mv SleepyLinux-amd64.hybrid.iso SleepyLinux-$DATUM-amd64.iso; md5sum SleepyLinux-$DATUM-amd64.iso > SleepyLinux-$DATUM-amd64.iso.md5; chown -R majorsza:majorsza SleepyLinux-*
 echo "ISO kész:"
 ls -lh *.iso
 
