5fd7c7bef5f3d083cd3ec245ab9011a07abb0adb
Szabi <majorsza@gmail.com>
2026-03-20 14:24:16 +0100

    2026-03-20@1424

--- build/build-sleepylinux-splash.sh
+++ build/build-sleepylinux-splash.sh
@@ -225,6 +225,11 @@ deb https://ftp.debian.org/debian trixie main contrib non-free non-free-firmware
 deb https://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
 deb https://ftp.debian.org/debian trixie-updates main contrib non-free non-free-firmware
 deb https://ftp.debian.org/debian trixie-backports main contrib non-free non-free-firmware
+
+# # # Sleepy.hu MIRROR
+#deb https://mirror.sleepy.hu/debian trixie main contrib non-free non-free-firmware
+#deb https://mirror.sleepy.hu/debian-security trixie-security main contrib non-free non-free-firmware
+#deb http://mirror.sleepy.hu/debian trixie-backports main contrib non-free non-free-firmware
 REPO
 
 apt update
