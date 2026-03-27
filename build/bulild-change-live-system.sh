87a8e331041b5c37c986dd28ff11eded3f8705bf
Szabi <majorsza@gmail.com>
2026-03-27 08:54:46 +0100

    2026-03-27@0854

--- build/bulild-change-live-system.sh
+++ build/bulild-change-live-system.sh
@@ -6,3 +6,4 @@ rm /DATA/LIVE/SleepyLinux/*.md5
 rm /DATA/LIVE/SleepyLinux/.build/binary_iso
 cd /DATA/LIVE/SleepyLinux/
 lb binary_iso
+DATUM=`date +%y.%m`; mv SleepyLinux-amd64.hybrid.iso SleepyLinux-$DATUM-amd64.iso; md5sum SleepyLinux-$DATUM-amd64.iso > SleepyLinux-$DATUM-amd64.iso.md5; chown -R majorsza:majorsza SleepyLinux-*
