e444c411299abaa4e0e3bb8091e008bcfc566ce8
Szabi <majorsza@gmail.com>
2026-03-27 08:37:11 +0100

    2026-03-27@0837

--- build/bulild-change-live-system.sh
+++ build/bulild-change-live-system.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sed -i 's/Live system/SleepyLinux Live/g' /DATA/LIVE/SleepyLinux/binary/boot/grub/grub.cfg
+rm /DATA/LIVE/SleepyLinux/*.iso
+rm /DATA/LIVE/SleepyLinux/*.md5
 rm /DATA/LIVE/SleepyLinux/.build/binary_iso
 cd /DATA/LIVE/SleepyLinux/
 lb binary_iso
