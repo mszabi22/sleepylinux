321a531f71d4f70bec8466463d5e48277b07cba1
Szabi <majorsza@gmail.com>
2026-04-16 08:40:52 +0200

    2026-04-16@0840

--- build/configs/root/add-programs.sh
+++ build/configs/root/add-programs.sh
@@ -117,6 +117,24 @@ Type=Application
 Categories=Office;
 Enabled=true" > /usr/share/applications/standard-notes.desktop
 ########################################################################
+echo "
+# apt #
+alias apt*='sudo apt autoremove '
+alias apt+='sudo apt install '
+alias apt-='sudo apt remove '
+alias update='sudo apt update; sudo apt upgrade; sudo chmod 777 ~/last_update.txt;sudo echo "Last update: `date +%Y.%m.%d.%H:%M`" > ~/last_update.txt'
+
+alias pwgen-readable='pwgen -B -s 8 10 -1 -0 -c -v'
+
+vdi2qcow() { qemu-img convert -p -f vdi -O qcow2 "$1" "${1%.vdi}.qcow2"; }
+qcow2resize() { qemu-img resize "$1" "$2"; }
+
+alias teszt-cpu='stress-ng --cpu 0 --timeout 300s --metrics'
+alias teszt-mem='stress-ng --vm 2 --vm-bytes 75% --timeout 300s --metrics'
+alias teszt-kombinalt='stress-ng --cpu 0 --vm 2 --vm-bytes 75% --hdd 1 --timeout 600s --metrics'
+" >> ~/.bash_aliases
+
+########################################################################
 SRC=/usr/share/images/desktop-base/desktop-grub.png
 cp $SRC /usr/share/plymouth/themes/sleepyhu/desktop-grub.png
 mkdir -p /boot/grub
