d5007a46d69d38e2cd79a160f69bd77f3b2db41a
Szabi <majorsza@gmail.com>
2026-04-20 12:09:56 +0200

    2026-04-20@1209

--- build/configs/root/add-programs.sh
+++ build/configs/root/add-programs.sh
@@ -122,12 +122,12 @@ echo "
 alias apt*='sudo apt autoremove '
 alias apt+='sudo apt install '
 alias apt-='sudo apt remove '
-alias update='sudo apt update; sudo apt upgrade; sudo chmod 777 ~/last_update.txt;sudo echo "Last update: `date +%Y.%m.%d.%H:%M`" > ~/last_update.txt'
+alias update='sudo apt update; sudo apt upgrade; sudo chmod 777 ~/last_update.txt;sudo echo \"Last update: `date +%Y.%m.%d.%H:%M`\" > ~/last_update.txt'
 
 alias pwgen-readable='pwgen -B -s 8 10 -1 -0 -c -v'
 
-vdi2qcow() { qemu-img convert -p -f vdi -O qcow2 "$1" "${1%.vdi}.qcow2"; }
-qcow2resize() { qemu-img resize "$1" "$2"; }
+vdi2qcow() { qemu-img convert -p -f vdi -O qcow2 \"$1\" \"${1%.vdi}.qcow2\"; }
+qcow2resize() { qemu-img resize \"$1\" \"$2\"; }
 
 alias teszt-cpu='stress-ng --cpu 0 --timeout 300s --metrics'
 alias teszt-mem='stress-ng --vm 2 --vm-bytes 75% --timeout 300s --metrics'
